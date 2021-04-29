115c41fb44d479c81999805e840d2206ca7688e7
Sergey Grozov <grozoff92@gmail.com>
2021-04-30 01:42:57 +0500

    Добавлены пустые тесты для контроллеров metrics manager

---
+++ MetricsManagerTests/HddMetricsControllerUnitTests.cs
@@ -0,0 +1,44 @@
+﻿using MetricsManager.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MetricsManagerTests
+{
+    public class HddMetricsControllerUnitTests
+    {
+        private HddMetricsController controller;
+
+        public HddMetricsControllerUnitTests()
+        {
+            controller = new HddMetricsController();
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnsOk()
+        {
+            //Arrange
+            var agentId = 1;
+
+            //Act
+            var result = controller.GetMetricsFromAgent(agentId);
+
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+
+        [Fact]
+        public void GetMetricsFromAllCluster_ReturnsOk()
+        {
+            //Arrange
+
+            //Act
+            var result = controller.GetMetricsFromAllCluster();
+
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
+}
---
+++ MetricsManagerTests/RamMetricsControllerUnitTests.cs
@@ -0,0 +1,44 @@
+﻿using MetricsManager.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MetricsManagerTests
+{
+    public class RamMetricsControllerUnitTests
+    {
+        private RamMetricsController controller;
+
+        public RamMetricsControllerUnitTests()
+        {
+            controller = new RamMetricsController();
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnsOk()
+        {
+            //Arrange
+            var agentId = 1;
+
+            //Act
+            var result = controller.GetMetricsFromAgent(agentId);
+
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+
+        [Fact]
+        public void GetMetricsFromAllCluster_ReturnsOk()
+        {
+            //Arrange
+
+            //Act
+            var result = controller.GetMetricsFromAllCluster();
+
+            // Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+    }
+}
