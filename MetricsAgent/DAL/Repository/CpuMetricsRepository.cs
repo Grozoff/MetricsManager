5b440633aa8c0e5633edb255a31120b05350bf16
Sergey <grozoff92@gmail.com>
2021-05-04 15:17:32 +0500

    Добавлен NuGet SQLlite Добавлены интерфейсы и модели для репозиториев Добавлены классы репозиториев

---
+++ MetricsAgent/DAL/Repository/CpuMetricsRepository.cs
@@ -0,0 +1,24 @@
+﻿using MetricsAgent.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using MetricsAgent.DAL.Interfaces;
+
+namespace MetricsAgent.DAL.Repository
+{
+    public class CpuMetricsRepository : ICpuMetricsRepository
+    {
+        private SQLiteConnection _connection;
+        public void Create(CpuMetric item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<CpuMetric> GetByTimePeriod(DateTimeOffset from, DateTimeOffset to)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MetricsAgent/DAL/Repository/DotNetMetricsRepository.cs
@@ -0,0 +1,22 @@
+﻿using MetricsAgent.DAL.Interfaces;
+using MetricsAgent.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsAgent.DAL.Repository
+{
+    public class DotNetMetricsRepository : ICpuMetricsRepository
+    {
+        public void Create(CpuMetric item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<CpuMetric> GetByTimePeriod(DateTimeOffset from, DateTimeOffset to)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
