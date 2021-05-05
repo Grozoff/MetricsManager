e70b36c142ca5d4c440cdeff055bbb5170b90630
Sergey <grozoff92@gmail.com>
2021-05-05 10:47:52 +0500

    Добавлены классы запросов и ответов для всех контроллеров агента

---
+++ MetricsAgent/Controllers/Requests/HddMetricRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsAgent.Controllers.Requests
+{
+    public class HddMetricRequest
+    {
+        public DateTimeOffset FromTime { get; set; }
+        public DateTimeOffset ToTime { get; set; }
+    }
+}
