32fdd9e9c5edbd059509afbf08ada16568f320c2
Sergey <grozoff92@gmail.com>
2021-05-25 20:01:50 +0500

    Добавлены запросы для контроллеров

---
+++ MetricsManager/Controllers/Requests/CpuMetricRequests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MetricsManager.Controllers.Requests
+{
+    public class CpuMetricFromAgentRequests
+    {
+        public CpuMetricFromAgentRequests(int agentId, DateTimeOffset fromTime, DateTimeOffset toTime)
+        {
+            AgentId = agentId;
+            FromTime = fromTime;
+            ToTime = toTime;
+        }
+
+        public CpuMetricFromAgentRequests()
+        {
+
+        }
+        public int AgentId { get; set; }
+        public DateTimeOffset FromTime { get; set; }
+        public DateTimeOffset ToTime { get; set; }
+    }
+    public class CpuMetricFromClusterRequests
+    {
+        public CpuMetricFromClusterRequests(DateTimeOffset fromTime, DateTimeOffset toTime)
+        {
+            FromTime = fromTime;
+            ToTime = toTime;
+        }
+
+        public CpuMetricFromClusterRequests()
+        {
+
+        }
+        public DateTimeOffset FromTime { get; set; }
+        public DateTimeOffset ToTime { get; set; }
+    }
+}
---
+++ MetricsManager/Controllers/Requests/NetworkMetricRequests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsManager.Controllers.Requests
+{
+    public class NetworkMetricFromAgentRequests
+    {
+        public NetworkMetricFromAgentRequests(int agentId, DateTimeOffset fromTime, DateTimeOffset toTime)
+        {
+            AgentId = agentId;
+            FromTime = fromTime;
+            ToTime = toTime;
+        }
+
+        public NetworkMetricFromAgentRequests()
+        {
+
+        }
+        public int AgentId { get; set; }
+        public DateTimeOffset FromTime { get; set; }
+        public DateTimeOffset ToTime { get; set; }
+    }
+    public class NetworkMetricFromClusterRequests
+    {
+        public NetworkMetricFromClusterRequests(DateTimeOffset fromTime, DateTimeOffset toTime)
+        {
+            FromTime = fromTime;
+            ToTime = toTime;
+        }
+
+        public NetworkMetricFromClusterRequests()
+        {
+
+        }
+        public DateTimeOffset FromTime { get; set; }
+        public DateTimeOffset ToTime { get; set; }
+    }
+}
